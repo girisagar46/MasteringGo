c33dd147554ddfb279c92891829d80c54cf6a15f
sagar.giri <sagar.giri@bebit.com>
2021-08-28 21:36:21 +0900

    :pig2: Change indent back to tabs using go fmt

--- src/myfirstapp/utility/loops.go
+++ src/myfirstapp/utility/loops.go
@@ -3,33 +3,32 @@ package utility
 import "fmt"
 
 func Looping() {
-    n := 1
-    for i:=10; i > 0; i-- {
-        n *= i
-    }
-    fmt.Println("Result: ", n)
+	n := 1
+	for i := 10; i > 0; i-- {
+		n *= i
+	}
+	fmt.Println("Result: ", n)
 
-    // There's no while loop in Go
-    n = 1
-    for n <= 50 {
-        n += n
-    }
-    fmt.Println("Result: ", n)
+	// There's no while loop in Go
+	n = 1
+	for n <= 50 {
+		n += n
+	}
+	fmt.Println("Result: ", n)
 
+	// Infinite loop
+	// WARN: DANGER
+	/*
+	   for {
+	       fmt.Println("Forever and ever")
+	   }
+	*/
 
-    // Infinite loop
-    // WARN: DANGER
-    /*
-    for {
-        fmt.Println("Forever and ever")
-    }
-    */
-
-    // defer inside loop
-    for i := 0; i < 5; i++ {
-        // WARN: Do not do defer inside loop
-        // ERROR: Possible resource leak, 'defer' is called in the 'for' loop
-        defer fmt.Print(i, " ") // Executed with LIFO basis. Output: 4 3 2 1 0
-    }
-    fmt.Println("Done")
+	// defer inside loop
+	for i := 0; i < 5; i++ {
+		// WARN: Do not do defer inside loop
+		// ERROR: Possible resource leak, 'defer' is called in the 'for' loop
+		defer fmt.Print(i, " ") // Executed with LIFO basis. Output: 4 3 2 1 0
+	}
+	fmt.Println("Done")
 }
--- src/projects/goRestClient/QuoteRestClient.go
+++ src/projects/goRestClient/QuoteRestClient.go
@@ -1,24 +1,24 @@
 package main
 
 import (
-    "fmt"
-    "io/ioutil"
-    "net/http"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
 func main() {
 
-    response, err := http.Get("http://quotes.rest/qod.json")
+	response, err := http.Get("http://quotes.rest/qod.json")
 
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
-    defer response.Body.Close() // this executes when function is exiting
-    contents, err := ioutil.ReadAll(response.Body)
-    if err != nil {
-        fmt.Println(err)
-        return // this will execute any defer statement that is stated in the code
-    }
-    fmt.Println(string(contents))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer response.Body.Close() // this executes when function is exiting
+	contents, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println(err)
+		return // this will execute any defer statement that is stated in the code
+	}
+	fmt.Println(string(contents))
 }
