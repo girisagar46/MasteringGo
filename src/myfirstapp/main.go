d75ab12feb24aea3e02652c63a7c619507b203ab
sagar.giri <sagar.giri@bebit.com>
2021-08-28 21:33:42 +0900

    :pig: Fix indent to space.

--- src/myfirstapp/main.go
+++ src/myfirstapp/main.go
@@ -1,76 +1,76 @@
 package main
 
 import (
-	"MasteringGo/src/myfirstapp/utility"
-	"fmt"
+    "MasteringGo/src/myfirstapp/utility"
+    "fmt"
 )
 
 func main() {
-	fmt.Println("Hello World!")
-	utility.SayHi()
-	//utility.sayHello() // this function is not accessible here
+    fmt.Println("Hello World!")
+    utility.SayHi()
+    //utility.sayHello() // this function is not accessible here
 
-	res, computeRes := utility.Inner()
-	fmt.Println(res, "", computeRes) // 3  50
+    res, computeRes := utility.Inner()
+    fmt.Println(res, "", computeRes) // 3  50
 
-	inc := utility.Increment()
-	fmt.Println(inc(), inc(), inc()) // 1 2 3
+    inc := utility.Increment()
+    fmt.Println(inc(), inc(), inc()) // 1 2 3
 
-	// Variables
-	var v1 int // variable declaration
-	v1 = 100 // assign value to variable v1
-	var v2 = 200 // declare and initialize variable (Should use var if declaring variable outside the function)
-	v3 := 300 // declare and initialize variable with type inference (This declaration only works inside the function)
-	fmt.Println(v1, v2, v3)
-
-
-	// Arrays are not flexible in Go, as we need to define size beforehand
-	var a [2]int
-	a[0], a[1] = 4, 5
-	fmt.Println("Array a: ", a, len(a))
-
-	b := [3]int{90, 99} // Empty space will be filled with zero because it's of int type
-	fmt.Println("Array b: ", b, len(b))
-
-	// Slice is more flexible than Arrays
-	c := []int{99, 55} // If we do not define size of Array, it becomes a slice
-	c = append(c, 88) // append takes slice as first input
-	c = append(c, 45)
-	fmt.Println("Slice c: ", c, len(c))
+    // Variables
+    var v1 int // variable declaration
+    v1 = 100 // assign value to variable v1
+    var v2 = 200 // declare and initialize variable (Should use var if declaring variable outside the function)
+    v3 := 300 // declare and initialize variable with type inference (This declaration only works inside the function)
+    fmt.Println(v1, v2, v3)
 
 
-	// Pointers
-	var p1 *int
-	num := 4
-	p1 = &num
-	fmt.Println(p1) // p1 stores tha address of num
+    // Arrays are not flexible in Go, as we need to define size beforehand
+    var a [2]int
+    a[0], a[1] = 4, 5
+    fmt.Println("Array a: ", a, len(a))
 
-	p2 := &num
-	fmt.Println(p2) // prints address where num is stored
-	fmt.Println(*p2) // prints the value of num. This is Dereferenceing the pointer value to the original one
+    b := [3]int{90, 99} // Empty space will be filled with zero because it's of int type
+    fmt.Println("Array b: ", b, len(b))
 
-	myVal := 5
-	fmt.Println("myVal Before", myVal)
-	utility.ChangeMe(&myVal) // We pass the address of myVal and in the ChangeMe function, we're deferencing it
-	fmt.Println("myVal After", myVal)
+    // Slice is more flexible than Arrays
+    c := []int{99, 55} // If we do not define size of Array, it becomes a slice
+    c = append(c, 88) // append takes slice as first input
+    c = append(c, 45)
+    fmt.Println("Slice c: ", c, len(c))
 
-	// type casting
-	n := 32
-	f := float64(n)
-	const pi = 3.1415 // This is constant value
-	fmt.Println(pi * f)
-	// Enum constants
-	const (
-		big int = 1 << 10
-		small = big >> 99
-	)
-	fmt.Println(big)
-	// big = 100 // this throws error because big is the constant.
-	fmt.Println(small)
 
-	// Flow Control in go. See utility.flowControl.go
-	utility.FlowControl()
+    // Pointers
+    var p1 *int
+    num := 4
+    p1 = &num
+    fmt.Println(p1) // p1 stores tha address of num
 
-	// Loops in Go. See utility.loops.go
-	utility.Looping()
+    p2 := &num
+    fmt.Println(p2) // prints address where num is stored
+    fmt.Println(*p2) // prints the value of num. This is Dereferenceing the pointer value to the original one
+
+    myVal := 5
+    fmt.Println("myVal Before", myVal)
+    utility.ChangeMe(&myVal) // We pass the address of myVal and in the ChangeMe function, we're deferencing it
+    fmt.Println("myVal After", myVal)
+
+    // type casting
+    n := 32
+    f := float64(n)
+    const pi = 3.1415 // This is constant value
+    fmt.Println(pi * f)
+    // Enum constants
+    const (
+        big int = 1 << 10
+        small = big >> 99
+    )
+    fmt.Println(big)
+    // big = 100 // this throws error because big is the constant.
+    fmt.Println(small)
+
+    // Flow Control in go. See utility.flowControl.go
+    utility.FlowControl()
+
+    // Loops in Go. See utility.loops.go
+    utility.Looping()
 }
--- src/myfirstapp/utility/utility.go
+++ src/myfirstapp/utility/utility.go
@@ -1,17 +1,17 @@
 package utility
 
 import (
-	"fmt"
+    "fmt"
 )
 
 func SayHi() {
-	fmt.Println("Hi!")
+    fmt.Println("Hi!")
 }
 
 // sayHello is not exportable function because the first letter of the function name is in lowercase
 // this function is private function
 func sayHello() {
-	fmt.Println("Hello!")
+    fmt.Println("Hello!")
 }
 
 // we do not need to write (a int, b int) as go understands both are int if we
@@ -20,50 +20,50 @@ func sayHello() {
 // be returned by this function and all return type is of int
 /*
 func mathematics(a, b int) (int, int, int) {
-	return a + b, a-b, a*b
+    return a + b, a-b, a*b
 }
 */
 
 // This declaration is more verbose and clear.
 // Here, in return type, we define variable names, and we just use the return keyword
 func mathematics(a, b int) (addition, subtraction, multiplication int) {
-	addition = a + b
-	subtraction = a - b
-	multiplication = a * b
-	return
+    addition = a + b
+    subtraction = a - b
+    multiplication = a * b
+    return
 }
 
 // ComputeAddAndMultiply function takes function as an argument
 // and execute it inside the function
 // here, `fn` is a function which matches the signature of `add` function
 func ComputeAddAndMultiply(val int, fn func(a, b int) int) int {
-	return val * fn(val, val)
+    return val * fn(val, val)
 }
 
 func Inner() (res, computeRes int){
-	// This is a function assigned to a variable (Function values)
-	// These type of function can be only defined inside another function
-	// See more: https://tour.golang.org/moretypes/24
-	add := func(a, b int) int {
-		return a+ b
-	}
-	// calling internally
-	res = add(1, 2)
-	computeRes = ComputeAddAndMultiply(5, add)
-	return
+    // This is a function assigned to a variable (Function values)
+    // These type of function can be only defined inside another function
+    // See more: https://tour.golang.org/moretypes/24
+    add := func(a, b int) int {
+        return a+ b
+    }
+    // calling internally
+    res = add(1, 2)
+    computeRes = ComputeAddAndMultiply(5, add)
+    return
 }
 
 // Increment closures
 // Here inner function is bound to tha variable and we're returning function
 func Increment() func() int {
-	i := 0
-	return func() int {
-		i++
-		return i
-	}
+    i := 0
+    return func() int {
+        i++
+        return i
+    }
 }
 
 // ChangeMe do not need to return anything, because we're directly working with the address
 func ChangeMe(X *int) {
-	*X = 10
+    *X = 10
 }
